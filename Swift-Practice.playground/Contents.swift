d0cdb10e3de41a732b6cfc9ee0262f9eaa05ab18
Shubhangi Gupta <shubhigupta991@gmail.com>
2020-11-07 00:37:58 +0530

    Update

---
+++ Swift-Practice.playground/Contents.swift
@@ -0,0 +1,23 @@
+// Hello eveyone. This is a comment
+
+/* This is
+  multi-line
+  comment */
+
+// String Interpolation
+
+print("Hello 2+3 world")
+print("Hello \(2+3) world")
+
+// Storing variables
+
+var a = 10
+var b = 20
+
+print(a,b)
+
+// Arrays
+
+var x = [1,2,3,4,5,6]
+
+print(x)
