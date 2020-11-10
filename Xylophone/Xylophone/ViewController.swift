324eba9f99e19eb246f9aec7a568567229bd684b
Shubhangi Gupta <shubhigupta991@gmail.com>
2020-11-11 01:32:56 +0530

    Update

---
+++ Xylophone/Xylophone/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Shubhangi Gupta on 11/11/20.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func CkeyPressed(_ sender: UIButton) {
+        playSound()
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
+
+}
+
