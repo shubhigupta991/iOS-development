9a6d333e0fdad8b6e7f9c123141b53b9134687c4
Shubhangi Gupta <shubhigupta991@gmail.com>
2020-11-12 23:43:06 +0530

    Update

---
+++ Swift/Intermediate.playground/Contents.swift
@@ -0,0 +1,28 @@
+// Functions
+
+func greetings() {print("Hello")}
+
+greetings()
+
+func tilte(resource: String) {
+    print(resource)
+}
+
+tilte(resource: "world!")
+
+// Control Flow
+
+var x = 0
+
+if x < 2 {print(x)}
+else {print("False")}
+
+func loveCalculator(){
+    let loveScore = Int.random(in: 0...100)
+    
+    if loveScore == 100{print("Kanye loves Kanye")}
+    else {"Forever alnoe"}
+}
+
+loveCalculator()
+
