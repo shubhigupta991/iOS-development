fee341ce97ae9417a2606d87b9ba63b56d989b12
Shubhangi Gupta <shubhigupta991@gmail.com>
2020-11-14 00:58:22 +0530

    Update

--- Egg Timer/Egg Timer/ViewController.swift
+++ Egg Timer/Egg Timer/ViewController.swift
@@ -6,40 +6,57 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 5, "Medium": 8, "Hard" : 12]
-
+    var player:  AVAudioPlayer?
+    
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard" : 7]
+    var secondsPassed = 0
+    var totalTime = 0
+    
+    var timer = Timer()
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var progressView: UIProgressView!
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
+        timer.invalidate()
         let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
         
-        let result = eggTimes[hardness]!
+        progressView.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
         
-        print(result)
-        var timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(UIMenuController.update), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
-        /*if hardness == "Soft" {
-            print(softTime)
-        }
-        else if hardness == "Medium"{
-            print(mediumTime)
+    }
+    func playSound(){
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
+
+    
+    @objc func updateTimer(){
+        if secondsPassed < totalTime {
+            
+            secondsPassed += 1
+            
+            let percentageProgress = Float(secondsPassed)/Float(totalTime)
+            progressView.progress = Float(percentageProgress)
+        
         }
         else {
-            print(hardTime)
-        }*/
-        
-        /*switch hardness {
-        case "Soft":
-            print(softTime)
-        case "Medium":
-            print(mediumTime)
-        case "Hard":
-            print(hardTime)
-        }*/
-    }
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+            playSound()
+        }
     
+        
+}
 }
 
