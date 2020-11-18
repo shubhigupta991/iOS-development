69b232a15d6867b4be73dc22bdb75a798568d4f4
Shubhangi Gupta <shubhigupta991@gmail.com>
2020-11-19 01:23:04 +0530

    Update

--- Swift/Intermediate.playground/Contents.swift
+++ Swift/Intermediate.playground/Contents.swift
@@ -72,3 +72,62 @@ var player1username: String? = nil     //defining optionals
 player1username = "benIsGreat"
 
 print(player1username!)           //unwrapping optionals
+
+// Structures
+
+struct Town{
+    let name = "MyLand"
+    var citizens = ["Shubhi", "John"]
+    var resources = ["Grain" : 100, "Ore" : 42, "Wool" : 75]
+    
+    func fortify(){
+        print("Increased Defense")
+    }
+}
+
+var Town1 = Town()
+Town1.citizens.append("Milly")
+print(Town1.citizens)
+Town1.fortify()
+
+struct SecondTown{
+    let name: String
+    var citizens: [String]
+    var resources: [String:Int]
+    
+    init(townName: String, people: [String], stats: [String:Int]) {
+        name = townName
+        citizens = people
+        resources = stats
+    }
+    
+    func fortify(){
+        print("Increased Defense")
+    }
+}
+
+var Town2 = SecondTown(townName: "DreamLand", people: ["Lily","Rose"], stats: ["Grains" : 50])
+Town2.citizens.append("Lotus")
+print(Town2.citizens.count)
+
+
+struct ThirdTown{
+    let name: String
+    var citizens: [String]
+    var resources: [String:Int]
+    
+    init(name: String, citizens: [String], resources: [String:Int]) {
+        self.name = name
+        self.citizens = citizens
+        self.resources = resources
+    }
+    
+    func fortify(){
+        print("Increased Defense")
+    }
+}
+
+var Town3 = ThirdTown(name: "Hill", citizens: ["Jack","Jill"], resources: ["Grains": 1000])
+Town3.resources["Ores"] = 76
+print(Town3.resources)
+
