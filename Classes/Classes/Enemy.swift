347a8e50536da297c3382d5f63efe8e2e5f6d060
Shubhangi Gupta <shubhigupta991@gmail.com>
2020-11-23 23:38:27 +0530

    Update

---
+++ Classes/Classes/Enemy.swift
@@ -0,0 +1,8 @@
+class Enemy {
+    var health = 100
+    var attackStrength = 10
+    
+    func move() {print("Walks Forward.")}
+    
+    func attack() {print("Land a hit, does \(attackStrength) damage")}
+}
---
+++ Classes/Classes/main.swift
@@ -0,0 +1,8 @@
+
+let skeleton = Enemy()
+print(skeleton.health)
+skeleton.move()
+skeleton.attack()
+
+let skeleton2 = Enemy()
+let skeleton3 = Enemy()
