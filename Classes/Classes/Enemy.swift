a04c200b810f3eae706ac443e0b709b1bcdf972a
Shubhangi Gupta <shubhigupta991@gmail.com>
2020-11-25 00:34:31 +0530

    Update Classes

--- Classes/Classes/Enemy.swift
+++ Classes/Classes/Enemy.swift
@@ -1,6 +1,15 @@
 class Enemy {
-    var health = 100
-    var attackStrength = 10
+    var health: Int
+    var attackStrength: Int
+    
+    init(health: Int, attackStrength: Int) {
+        self.health = health
+        self.attackStrength = attackStrength
+    }
+    
+    func takeDamage(amount: Int) {
+        self.health -= amount
+    }
     
     func move() {print("Walks Forward.")}
     
--- Classes/Classes/main.swift
+++ Classes/Classes/main.swift
@@ -1,12 +1,14 @@
 
-let skeleton = Enemy()
+let skeleton = Enemy(health: 100, attackStrength: 10)
 print(skeleton.health)
 skeleton.move()
 skeleton.attack()
 
-print()
+let skeleton2 = skeleton
+skeleton.takeDamage(amount: 10)
+print(skeleton2.health)
 
-let dragon = Dragon()
+let dragon = Dragon(health: 100, attackStrength: 10)
 dragon.wingSpan = 5
 dragon.attackStrength = 15
 dragon.talk(speech: "My Teeth are swords! My claws are spears! My wings are hurricane")
