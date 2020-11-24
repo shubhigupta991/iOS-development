d593fefb02c7c8e0bcf8df2b1c7955ead9d602b1
Shubhangi Gupta <shubhigupta991@gmail.com>
2020-11-24 23:54:42 +0530

    Update

--- Classes/Classes/main.swift
+++ Classes/Classes/main.swift
@@ -4,5 +4,11 @@ print(skeleton.health)
 skeleton.move()
 skeleton.attack()
 
-let skeleton2 = Enemy()
-let skeleton3 = Enemy()
+print()
+
+let dragon = Dragon()
+dragon.wingSpan = 5
+dragon.attackStrength = 15
+dragon.talk(speech: "My Teeth are swords! My claws are spears! My wings are hurricane")
+dragon.move()
+dragon.attack()
