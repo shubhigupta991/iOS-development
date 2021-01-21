132821bc50b32f872c0ec7d044ff91e1bcaae5e2
Shubhangi Gupta <shubhigupta991@gmail.com>
2021-01-21 23:14:48 +0530

    Update RegisterViewController.swift

--- Flash-Chat/Flash-Chat/Controllers/RegisterViewController.swift
+++ Flash-Chat/Flash-Chat/Controllers/RegisterViewController.swift
@@ -7,13 +7,30 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        errorLabel.text = ""
+    }
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    self.errorLabel.text = e.localizedDescription
+                }else {
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+                
+            }
+        }
     }
     
 }
