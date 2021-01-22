4ce20b728d712e8ab70d124ee54dd0466b086cf1
Shubhangi Gupta <shubhigupta991@gmail.com>
2021-01-22 20:48:21 +0530

    Added Constants.swift

--- Flash-Chat/Flash-Chat/Controllers/LoginViewController.swift
+++ Flash-Chat/Flash-Chat/Controllers/LoginViewController.swift
@@ -26,7 +26,7 @@ class LoginViewController: UIViewController {
                 if let e = error {
                     self.errorLabel.text = e.localizedDescription
                 }else {
-                    self.performSegue(withIdentifier: "LoginToChat" , sender: self)
+                    self.performSegue(withIdentifier: K.loginSegue , sender: self)
                 }
             }
         }
--- Flash-Chat/Flash-Chat/Controllers/RegisterViewController.swift
+++ Flash-Chat/Flash-Chat/Controllers/RegisterViewController.swift
@@ -26,7 +26,7 @@ class RegisterViewController: UIViewController {
                 if let e = error {
                     self.errorLabel.text = e.localizedDescription
                 }else {
-                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                    self.performSegue(withIdentifier: K.registerSegue, sender: self)
                 }
                 
             }
