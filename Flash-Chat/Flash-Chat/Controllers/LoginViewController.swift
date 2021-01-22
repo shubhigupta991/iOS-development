a0b4e445fb428d0ef4c7eb73ad0aea2b668fbc41
Shubhangi Gupta <shubhigupta991@gmail.com>
2021-01-22 19:57:14 +0530

    Update LoginViewController.swift

--- Flash-Chat/Flash-Chat/Controllers/LoginViewController.swift
+++ Flash-Chat/Flash-Chat/Controllers/LoginViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
     
+    override func viewDidLoad() {
+        errorLabel.text = ""
+    }
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    self.errorLabel.text = e.localizedDescription
+                }else {
+                    self.performSegue(withIdentifier: "LoginToChat" , sender: self)
+                }
+            }
+        }
     }
     
 }
