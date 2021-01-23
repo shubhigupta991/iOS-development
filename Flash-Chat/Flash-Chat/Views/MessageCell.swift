253bd76b2eb07eb5958285ae8366b2811bc9c08d
Shubhangi Gupta <shubhigupta991@gmail.com>
2021-01-23 23:47:29 +0530

    Added Customised Cells

---
+++ Flash-Chat/Flash-Chat/Views/MessageCell.swift
@@ -0,0 +1,27 @@
+//
+//  MessageCell.swift
+//  Flash-Chat
+//
+//  Created by Shubhangi Gupta on 23/01/21.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
