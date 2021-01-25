dcf892e0e71e22b4d801651f061828149c92f473
Shubhangi Gupta <shubhigupta991@gmail.com>
2021-01-25 23:16:59 +0530

    Update

--- Swift.playground/Contents.swift
+++ Swift.playground/Contents.swift
@@ -19,3 +19,32 @@ class Fish: Animal {
         print("Breathing under water")
     }
 }
+
+let shubhi = Human(n: "Shubhi")
+let jack = Human(n: "Jack")
+let nemo = Fish(n: "Nemo")
+
+let neighbours = [shubhi,jack,nemo]
+
+// is as! as? keyword
+if neighbours[0] is Human {
+    print("First Neighbour is human")
+}
+
+func findNemo(from animals: [Animal]) {
+    for animal in animals {
+        if animal is Fish {
+            print(animal.name)
+            let fish = animal as! Fish
+            fish.breathTaking()
+        }
+    }
+}
+
+findNemo(from: neighbours)
+
+if let fish = neighbours[1] as? Fish {
+    fish.breathTaking()
+}else {
+    print("Casting has failed")
+}
